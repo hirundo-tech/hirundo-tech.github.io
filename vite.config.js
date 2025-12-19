1b4ce87d7b07b163e136c8b5ebfe4a2fc39de6a9
SAJID MASOOD <53954375+sajidmasoodbscs@users.noreply.github.com>
2025-12-20 00:48:21 +0500

    Set base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
+hirundo-tech.github.io
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/hirundo-tech.github.io/", 
   plugins: [react(), tailwindcss()],
 });
